c0cadffbd24b0def83bdc9274434cff669c72962
kirill <spatarikirill@gmail.com>
2020-11-11 01:32:29 +0200

    complited the task with nokogiri

--- testtask.rb
+++ testtask.rb
@@ -2,9 +2,10 @@ require 'watir'
 require 'active_support/time'
 require_relative 'account'
 require_relative 'transaction'
+require 'nokogiri'
 
 class Scraper
-    attr_accessor :accounts, :browser, :transactions, :transactiondates
+    attr_accessor :accounts, :browser, :transactions
   
     def openBrowser # use chrome browser
       @browser = Watir::Browser.new(:chrome)
@@ -18,46 +19,47 @@ class Scraper
       @browser.button(class: "input_submit", text: "Launch Business Demo").click
       @browser.li("data-semantic" => "account-item").wait_until(&:present?)
     end
-   
-    def extractAccountsData
 
-      @accounts = browser.lis("data-semantic" => "account-item").map do |item|
-        currency = get_currency( item.span("data-semantic" => "available-balance").text)
-        nature = get_nature(item.div("data-semantic" => "account-name").text)
+    def extractAccountsData
+     parsed_page = Nokogiri::HTML.parse(browser.html)
+     @accounts = parsed_page.css("li[data-semantic='account-item']").map do |item|
+        currency = get_currency(item.css("span[data-semantic='available-balance']").text)
+        nature = get_nature(item.css("div[class='css-aralps']").text)
         Account.new(
-          item.div("data-semantic" => "account-name").text,
+          item.css("div[class='css-aralps']").text,
           currency,
-          item.span("data-semantic" => "available-balance").text.each_char.map { |char| char[/\d|[.]/] }.join.to_f,
+          item.css("span[data-semantic='available-balance']").text.each_char.map { |char| char[/\d|[.]/] }.join.to_f,
           nature
         )
-      end
-    end   
+      end   
+    end
 
       def accessTransactionsPage
         @transactions = []
         @accounts.each do |account|
-        @browser.div(class: "css-aralps", text: account.name).parent.parent.parent.parent.click
-        @browser.link(class: "segmented-control__item", text: "Activity").wait_until(&:present?)
-        @browser.link(class: "segmented-control__item", text: "Activity").click
-        @browser.link(class: "_1hzCXGy_Ye", text: "Filter").wait_until(&:present?)
-        @browser.link(class: "_1hzCXGy_Ye", text: "Filter").click
-        @browser.link(class: "panel--bordered__item", text: "All time").wait_until(&:present?)
-        @browser.link(class: "panel--bordered__item", text: "All time").click
-        @browser.span("data-semantic" => "name", text: "Custom Date Range").parent.parent.parent.parent.parent.parent.click
-        @browser.text_field("data-semantic" => "filter-from-date-input").set((Time.now - 2.month).strftime("%d/%m/%Y"))
-        @browser.text_field("data-semantic" => "filter-to-date-input").set((Time.now).strftime("%d/%m/%Y"))
-        @browser.button(class: "button--primary", text: "Apply Filter").click
-        @browser.button(class: "button--primary", text: "Apply Filters").click
-        sleep 3
+          @browser.div(class: "css-aralps", text: account.name).parent.parent.parent.parent.click
+          @browser.link(class: "segmented-control__item", text: "Activity").wait_until(&:present?)
+          @browser.link(class: "segmented-control__item", text: "Activity").click
+          @browser.link(class: "_1hzCXGy_Ye", text: "Filter").wait_until(&:present?)
+          @browser.link(class: "_1hzCXGy_Ye", text: "Filter").click
+          @browser.link(class: "panel--bordered__item", text: "All time").wait_until(&:present?)
+          @browser.link(class: "panel--bordered__item", text: "All time").click
+          @browser.span("data-semantic" => "name", text: "Custom Date Range").parent.parent.parent.parent.parent.parent.click
+          @browser.text_field("data-semantic" => "filter-from-date-input").set((Time.now - 2.month).strftime("%d/%m/%Y"))
+          @browser.text_field("data-semantic" => "filter-to-date-input").set((Time.now).strftime("%d/%m/%Y"))
+          @browser.button(class: "button--primary", text: "Apply Filter").click
+          @browser.button(class: "button--primary", text: "Apply Filters").click
+          sleep 3
+          parsed_page = Nokogiri::HTML.parse(browser.html)
 
-          @browser.lis("data-semantic" => "activity-group").each do |dates|
-      
-            dates.lis("data-semantic" => "activity-item").each do |item| 
-              currency = get_currency(item.span("data-semantic" => "amount").text)
+          parsed_page.css("li[class='grouped-list__group']").each do |dates|
+            #puts dates.text
+            dates.css("li[data-semantic='activity-item']").each do |item|
+              currency = get_currency(item.css("span[data-semantic='amount']").text)
               @transactions << Transaction.new(
-                  dates.h3(class: "grouped-list__group__heading").text,
-                  item.span(class: "overflow-ellipsis").text,
-                  item.span(class: "amount").text.each_char.map { |char| char[/\d|[.]|[-]|[+]/] }.join.to_f,
+                  dates.css("h3[class='grouped-list__group__heading']").text, #date
+                  item.css("span[class='overflow-ellipsis']").text, #description
+                  item.css("span[data-semantic='amount']").text.each_char.map { |char| char[/\d|[.]|[-]|[+]/] }.join.to_f, #amount
                   currency,
                   account.name
                 )
